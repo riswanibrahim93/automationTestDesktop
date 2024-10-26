65f6cbbd4d3f931c813a8ceae8d66f20e9c95749
riswanibrahim93 <ibrahim93vikings@gmail.com>
2024-10-26 14:44:59 +0700

    commit_first_automationDesktop

---
+++ Test Suites/SOAL AUTOMATION DESKTOP/Ms Word Suite Test.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Ms Word Suite Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f689fb0c-2c86-430d-9d16-b2a632a4d58d</testSuiteGuid>
+   <testCaseLink>
+      <guid>4656c0cd-940d-46a1-aa8a-371408e5093a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SOAL AUTOMATION DESKTOP/MS Word Test Save Positif</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>f99718ff-c4aa-4680-a6ab-f51bda7263f5</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data Driven/Data Input Word</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>f99718ff-c4aa-4680-a6ab-f51bda7263f5</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Input 1</value>
+         <variableId>709fb398-2957-4b04-9364-9d72ef41ff78</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>f99718ff-c4aa-4680-a6ab-f51bda7263f5</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Input 2</value>
+         <variableId>7458c4a9-4289-4ee4-8daf-c5ac6e0e7fce</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>f99718ff-c4aa-4680-a6ab-f51bda7263f5</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Input 3</value>
+         <variableId>f24b0339-5d26-46d4-8e57-71d3ad99cddc</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
